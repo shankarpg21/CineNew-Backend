b389be1153b10fd3cc7ecbb7754e8ddc1c0483ef
shankarpg21 <shankarpg57@gmail.com>
2026-09-14 16:09:52 +0530

    DeployToCloud

--- src/main/java/com/example/Cinenew_backend/showseat/ShowSeat.java
+++ src/main/java/com/example/Cinenew_backend/showseat/ShowSeat.java
@@ -17,7 +17,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.LockModeType;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
